639dadfc74a72a2eb07f0dad3b057b146d6dc3e4
Joris Röling <j.roling@tmg.nl>
2017-12-22 17:45:53 +0100

    log

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 // const url = require('url');
-// const yves = require('yves');
+const yves = require('yves');
 const request = require('request');
 const app = express();
 const apicache = require('apicache')
 const cache = apicache.middleware
 
 app.get('/content/*', cache('5 minutes'), function(req, res){
-  // yves(req.originalUrl)
+  yves(req.originalUrl)
   request.get('https://raw.githubusercontent.com/codusnl/website/master'+req.originalUrl).pipe(res);
   // const parsed = url.parse(req.originalUrl)
   // res.sendFile(parsed.pathname, {root: '.'});
